b852f4797e9f0e8c61c10b4e5219462cbfa95e82
MuhammadFauzi <92508010+oneup1945@users.noreply.github.com>
2022-12-17 17:41:45 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println();
+        Barang input = new Barang();
+         input.setNamaBarang("kopi");
+         input.setKodeBarang(12);
+         input.setStokBarang(30);
+         input.setKeterangan("Barang berharga");
+
+         User pengguna = new User();
+         pengguna.setUsername("Ahmad");
+         pengguna.setAlamat("Yogyakarta");
+         pengguna.setIdUser(1242);
+
+         TransaksiMasuk masuk = new TransaksiMasuk();
+         masuk.setKodeMasuk(1243);
+         masuk.setKodeBarang(123);
+
+
+         TransaksiKeluar keluar = new TransaksiKeluar();
+         keluar.setKodeKeluar(13345);
+         keluar.setKodeBarang(123);
+         keluar.tanggal();
+         keluar.setJumlahKeluar(30);
+         keluar.deskribsi();
+
+        System.out.println("=============================");
+        System.out.println(" nama Barang :"+input.getNamaBarang());
+        System.out.println(" Kode Barang : "+input.getKodeBarang());
+        System.out.println(" Stok Barang : "+input.getStokBarang());
+        System.out.println(" Keterangan  : "+input.getKeterangan());
+        System.out.println("============================== ");
+        System.out.println(" Username : "+pengguna.getUsername());
+        System.out.println(" alamat   : "+pengguna.getAlamat());
+        System.out.println(" id User  : "+pengguna.getIdUser());
+        System.out.println("============================== ");
+        System.out.println("**********TRANSAKSI MASUK*******");
+        System.out.println(" Kode Transaksi Masuk   : "+masuk.getKodeMasuk());
+        System.out.println(" Id Pengguna            : "+pengguna.getUsername());
+        System.out.println(" Kode Barang            : "+input.getKodeBarang());
+        System.out.println(" tanggal barang masuk   : "+masuk.tanggal());
+        System.out.println(" Jumlah barang          : "+input.getStokBarang());
+        System.out.println(" Deskribsi              : "+masuk.deskribsi());
+        System.out.println("===================================");
+        System.out.println("********TRANSAKSI KELUAR********");
+        System.out.println(" Kode Transaksi Keluar   : "+keluar.getKodeKeluar());
+        System.out.println(" Id Pengguna             : "+pengguna.getIdUser());
+        System.out.println(" Kode Barang             : "+input.getKodeBarang());
+        System.out.println(" tanggal Barang Keluar   : "+keluar.tanggal());
+        System.out.println(" Jumlah barang           : "+input.getStokBarang());
+        System.out.println(" Keterangan              : "+keluar.deskribsi());
+        System.out.println("====================================");
+
+        System.out.println("================================================");
+        System.out.println("############# DATA KARYAWAN ####################");
+        System.out.println("Masukkan jenis karyawan(tetap/harian): ");
+        String jenis = sc.nextLine();
+        System.out.println("Masukkan nama Karyawan: ");
+        String nama = sc.nextLine();
+        System.out.println("Masukkan ID karyawan");
+        String id = sc.nextLine();
+
+        Karyawan karyawan;
+        if(jenis.equals("tetap")){
+            System.out.println("Masukkan gaji pokok karyawan: ");
+            int gajiPokok = sc.nextInt();
+            System.out.print("Masukkan tunjangan karyawan: ");
+            int tunjangan = sc.nextInt();
+
+            karyawan = new KaryawanTetap();
+            karyawan.nama = nama;
+            karyawan.id = id;
+            ((KaryawanTetap) karyawan).gajiPokok = gajiPokok;
+            ((KaryawanTetap) karyawan).tunjangan = tunjangan;
+        }else {
+            System.out.print("Masukkan jumlah jam kerja karyawan: ");
+            int jumlahJamKerja = sc.nextInt();
+            System.out.print("Masukkan tarif per jam karyawan: ");
+            int tarifPerJam = sc.nextInt();
+            System.out.print("Masukkan tunjangan karyawan: ");
+            int tunjangan = sc.nextInt();
+
+            karyawan = new KaryawanHarian();
+            karyawan.nama = nama;
+            karyawan.id = id;
+            ((KaryawanHarian) karyawan).jumlahJamKerja = jumlahJamKerja;
+            ((KaryawanHarian) karyawan).tarifPerJam = tarifPerJam;
+            ((KaryawanHarian) karyawan).tunjangan = tunjangan;
+        }
+        karyawan.info();
+        System.out.println("Gaji yang diterima: " + karyawan.hitungGaji());
+
+
+        if (karyawan instanceof Gaji) {
+            System.out.println("Tunjangan yang diterima: " + ((Gaji) karyawan).hitungTunjangan());
+        }
+
+        sc.close();
+    }
+}
